691a69db996327623f95c5f4581ce277718f4166
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-09 23:45:51 +0300

    return deyimi incelendi.

--- src/com/company/Metotlar.java
+++ src/com/company/Metotlar.java
@@ -2,16 +2,38 @@ package com.company;
 
 public class Metotlar {
     public static void main(String[] args) {
-     mesajVer(); // staticte üretmene gerek yok nesneyi.
-     Metotlar method = new Metotlar(); // nesne oluşturma. ClassName cs = new Classname
+        mesajVer("aa"); // staticte üretmene gerek yok nesneyi.
+        mesajVer("bb");
+        Metotlar method = new Metotlar(); // nesne oluşturma. ClassName cs = new Classname
         method.hesapla();
+        System.out.println(topla(6, 3));
+
+        System.out.println(toplananYuzdesi(10,90));
+
     }
 
-    public static  void mesajVer(){
-        System.out.println("Belhanda");
+    public static void mesajVer(String isim) {
+        System.out.println("Belhanda" + " " + isim);
     }
 
-    public  void hesapla(){
+    public void hesapla() {
         System.out.println("10");
+    }
+
+    public static int topla(int sayi1, int sayi2) {
+        int toplam = sayi1 + sayi2;
+        return toplam;
+        //System.out.println("Toplam : = "+toplam);
+    }
+
+    public static double toplananYuzdesi(int sayi1, int sayi2) {
+        int toplam = topla(sayi1, sayi2);
+        double sonuc = toplam * 10 / 100;
+        return sonuc;
+    }
+
+    public static double krediHesapla() {
+        //hesaplar yapılır
+        return 125000;
     }
 }
