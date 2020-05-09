9958b7401e99a4f90208d5c9b3c694b5aa9d062f
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-09 21:46:08 +0300

    Degiskenler tamamlandi.

---
+++ src/com/company/Degiskenler.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Degiskenler {
+    public static void main(String[] args) {
+        System.out.println("Kubilay");
+
+        int not =55;
+
+        int kredi=100000;
+
+        double notOrtalamasi=50.5;
+
+        String isim ="Belhanda";
+
+        boolean isLog=false;
+
+        System.out.println(not);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,4 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Kubilay");
-    }
 }
